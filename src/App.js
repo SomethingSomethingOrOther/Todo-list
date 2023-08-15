1352b2f0cd32a86414ae5ef16f1278196476e91e
SomethingSomethingOrOther <abdoulgdia@gmail.com>
2023-08-15 18:27:22 -0400

    added updatetodo functions and handleEdit function

--- src/App.js
+++ src/App.js
@@ -1,66 +1,84 @@
-import './App.css';
-import {useState} from "react"
-
-
+import { useState } from "react"
 
 function App() {
-  const [inputValue,setInputValue]=useState("")
-  const [todosArray,setTodosArray] = useState([])
+  const [inputValue, setInputValue] = useState("")
+  const [editInputValue, setEditInputValue] = useState("")
+  const [todosArray, setTodosArray] = useState([])
 
-    const handleInputChange=(e)=>{
-        setInputValue(e.target.value)
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const addToList = (value) => {
+    const newTodosArray = [...todosArray, value]
+    if (inputValue !== "" && !todosArray.includes(inputValue)) {
+      setTodosArray(newTodosArray)
+      setInputValue("") 
     }
-   const addToList=(value)=>{
-    const newTodosArray=[...todosArray,value]
-      if (inputValue !==""&& !todosArray.includes(inputValue)){
+  }
 
-        setTodosArray(newTodosArray)
-      }
-}
+  const cancelCurrentInput = () => {
+    setInputValue("")
+  }
 
+  const removeFromList = (index) => {
+    const newTodosArray = todosArray.filter((_, i) => i !== index)
+    setTodosArray(newTodosArray)
+  }
 
-const cancelCurrentInput=()=>{
-  setInputValue("")
-}
+  const handleEdit = (index) => {
+    setEditInputValue(todosArray[index])
+    removeFromList(index)
 
-const removeFromList=(value)=>{
-  const newTodosArray= todosArray.filter((todo)=>{
-    return todo.id  !== value.id
-  })
-  setTodosArray(newTodosArray)
-}
+  }
 
-
-
+  const updateTodo = (index) => {
+    const newTodosArray = [...todosArray]
+    newTodosArray.splice(index, 0, editInputValue)
+    setTodosArray(newTodosArray)
+    setEditInputValue("") 
+  }
 
   return (
     <div className="App">
-       <div>
+      <div>
         <h1>
           Todo List
         </h1>
-       </div>
-       <div>
-        <input value={inputValue} 
-        onChange={handleInputChange} 
-        type="text" />
+      </div>
+      <div>
+
+        {editInputValue ? (
+        <input 
+          placeholder="Edit your todo..."
+          name="editinputvalue"
+          type="text"
+          value={editInputValue}
+          onChange={(e) => setEditInputValue(e.target.value)}
+        />):(<input placeholder="Write a goal..."
+        name="inputvalue"
+        value={inputValue}
+        onChange={handleInputChange}
+        type="text"
+      />)
+      }
         <div>
-          <button onClick={()=>addToList(inputValue)}>Add</button> {" "} 
+          <button onClick={() => addToList(inputValue)}>Add</button>{" "}
           <span><button onClick={cancelCurrentInput}>Cancel</button></span>
-          <button style={{display:"none"}}>Update</button>
+          <button onClick={updateTodo} style={{ display: editInputValue ?  "inline-block": "none" }}>Update</button>
         </div>
-       </div>
-       <div className="list">
-            {
-              todosArray.map((todo,i)=>(
-                <>
-                  <p key={todo.id}>{todo}</p>
-                  <button>Edit</button> {" "}
-                  <span><button onClick={()=>removeFromList(i)}>Delete</button></span>
-                </>
-              ))
-            }
-       </div>
+      </div>
+      <div className="list">
+        {
+          todosArray.map((todo, i) => (
+            <div key={i}>
+              <p>{todo}</p>
+              <button onClick={() => handleEdit(i)}>Edit</button>{" "}
+              <span><button onClick={() => removeFromList(i)}>Delete</button></span>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
